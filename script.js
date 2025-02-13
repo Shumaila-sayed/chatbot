43616528a4b1d2136d8bca8d0e3aa3551093dd20
Shumaila <sayedshumaila27@gmail.com>
2025-02-13 06:05:48 +0530

    Get Chatbot response

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const userMessage = document.querySelector('.message-input');
 const chatBody = document.querySelector('.chat-body'); 
 
-
 let user = {
     text: null
 }
@@ -28,4 +27,29 @@ function displayText(message) {
     textDiv.appendChild(text);
 
     chatBody.appendChild(textDiv)
+}
+
+
+
+async function generateBotResponse(message) {
+    const API_KEY = 'AIzaSyDX7I56dPaMUZsxuvuE09W4EeXV6CXvV8A';
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+    
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                contents: [
+                    { parts: [{ text: message }] },
+                ],
+            }),
+        })
+
+        const json = await response.json();
+        const botReply = json.candidates[0].content.parts[0].text
+        return botReply;
+    } catch (err) {
+        console.log(err)
+    }
 }
